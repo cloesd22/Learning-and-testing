64e0efefdf12ebb9c887fda4d832fde92e8fdb6a
cloesd22 <cloesd22@gmail.com>
2016-10-31 04:26:41 +1100

    some other stuff

--- Javascript/scripts/flyingmonkesy.js
+++ Javascript/scripts/flyingmonkesy.js
@@ -4,6 +4,8 @@ ageNUM = Number(age);
 
 
 var headingtag = document.getElementsByTagName("h3")[0];
+var SentenceSw = document.getElementsByTagName("p")[0];
+
 
 
 throb = setInterval(changename,1000);
@@ -14,34 +16,34 @@ function changename(){
 
     switch (ageNUM)
         {
-        case 15:
-               
-        headingtag.innerHTML = "Ya";
+            case 15:
+
+                headingtag.innerHTML = "Ya";
+                SentenceSw.innerHTML="Ya is a test to see if anything loads before the head tester";
                 document.body.style.background = "white";
-                
-                 ageNUM=ageNUM+1;
-        break;
-                
-        case 16:
-               
-        headingtag.innerHTML = "Bing";
-               
-                 ageNUM=ageNUM+1;
-        break;
-                
-                case 17:
-               
-        headingtag.innerHTML = "Doogle";
-               
-                 ageNUM=ageNUM+1;
-        break;
-                
-                case 18:
-                
-        headingtag.innerHTML = "Doo";
-                
+                ageNUM=ageNUM+1;
+            break;
+
+            case 16:
+
+                headingtag.innerHTML = "Bing";
+                SentenceSw.innerHTML="Ya Bing a test to see if anything loads before the head tester";       
+                ageNUM=ageNUM+1;
+            break;
+
+            case 17:
+
+                headingtag.innerHTML = "Doogle";
+                SentenceSw.innerHTML="Ya Bing a test to see if Doogle loads before the head tester";          
+                ageNUM=ageNUM+1;
+            break;
+
+            case 18:
+
+                headingtag.innerHTML = "Doo";
+                SentenceSw.innerHTML="Ya Bing a test to see if Doogle loads before the Doo tester";         
                 ageNUM=15;
-        break;
+            break;
     
         }
 
