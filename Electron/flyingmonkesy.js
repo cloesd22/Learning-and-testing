6e482f9cb52706f093b6bcbb8eeb9db3547cfa85
cloesd22 <cloesd22@gmail.com>
2016-11-01 23:55:43 +1100

    example 3 of the javascript tutorial

--- Electron/flyingmonkesy.js
+++ Electron/flyingmonkesy.js
@@ -11,6 +11,11 @@ var germanyPic = document.getElementById("germanypic");
 var icelandPic = document.getElementById("icelandpic");
 var chinaPic = document.getElementById("chinapic");
 
+var checklist = document.getElementById("stufftobring");
+var listitems = checklist.querySelectorAll("li"); 
+var inputs = checklist.querySelectorAll("input");
+var spans = checklist.querySelectorAll("span");
+
 var placevalue = placePick.options[placePick.selectedIndex].value;
 
 
@@ -19,7 +24,43 @@ numOne.addEventListener("input",add);
 numTwo.addEventListener("input",add);
 colorin.addEventListener("input",colorchange);
 
+for (i=0; i<lists.length;i++)
+    {
+        listitems[i].addEventListener("click", showfield)
+        inputs[i].addEventListener("blur", blurfield)
+        inputs[i].addEventListener("keypress", itemKeypress)
+     
+        
+    }
 
+
+function showfield(){
+    
+   
+    this.className="edit";
+    var input = this.querySelector("input");
+    input.focus();
+    input.setSelectionRange(0,input.value.length);
+    
+
+}
+
+function blurfield(){
+    
+    this.parentNode.className="";
+    this.previousElementSibling.innerHTML=this.value;
+}
+
+function itemKeypress(event){
+    console.log(event);
+    
+    if (event.which==13){
+        this.parentNode.className="";
+        this.previousElementSibling.innerHTML=this.value;
+        
+    }
+    
+}
 
 
 function add()
@@ -40,29 +81,33 @@ function colorchange(){
 
 placePick.addEventListener("input",showpic);
                            
-function showpic()
-{
-    var allimages = document.querySelectorAll("img");
-    
-    for (i=0;i<allimages.length;i++)
-        {
-            allimages[i].className="hide";
-            
-        }
-    
-    
-    var picid = (this.options[this.selectedIndex].value);
-    
-    var pic = document.getElementById(picid);
-        
-    if (pic.className==="hide"){
-        pic.className="";
-        
-    }else{
-        pic.className="hide";    
-}
-    
-    
+    function showpic()
+    {
+        var allimages = document.querySelectorAll("img");
 
-}
+        for (i=0;i<allimages.length;i++)
+            {
+                allimages[i].className="hide";
+
+            }
+
+
+        var picid = (this.options[this.selectedIndex].value);
+
+        var pic = document.getElementById(picid);
+
+        if (pic.className==="hide"){
+            pic.className="";
+
+        }else{
+            pic.className="hide";    
+        }
+
+
+
+    }
+
+
+
+
 
--- Electron/index.js
+++ Electron/index.js
@@ -7,7 +7,7 @@ app.on('ready',function(){
 var mainWindow=new BrowserWindow({
 width:800,
 height:600,
-frame:false
+
 
 })
 mainWindow.loadURL('file:///E:/Electronus/Gittesting/Electron/livetest.html')
