27521244e5a7a89fb35719de4f09bfcd4f813a78
cloesd22 <cloesd22@gmail.com>
2016-11-06 05:14:07 +1100

    added asp.net and MVC default filesfolders

---
+++ ASP.net/TutorialX1/TutorialX1/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="TutorialX1.Global" Language="C#" %>
---
+++ ASP.net/WebApplication1/WebApplication1/Controllers/HomeController.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebApplication1.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET /home/index
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        //GET /home/about
+        //So inside the controllers folder there is a 'home' controller. anything that uses /home/ gets routed through there.
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Call me men";
+
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Contact(int message)
+        {
+            ViewBag.Message = "Thanks we got it men";
+
+            return View();
+        }
+
+        
+        public ActionResult Foo()
+        {
+            
+            return View("About");
+        }
+
+        public ActionResult Serial(string letterCase)
+        {
+            var serial = "ASPNETMVC5ATM1";
+
+            if (letterCase == "lower")
+            {
+                return Content(serial.ToLower());
+            }
+
+            // return new HttpStatusCodeResult(403);
+            return Json(new { name = "serial", value = serial }, JsonRequestBehavior.AllowGet);
+
+
+        }
+
+
+    }
+}
